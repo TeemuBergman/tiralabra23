ce417a5b1797e747b50216505ef2b29e8238fa5b
Teemu Bergman <teemu.bergman@helsinki.fi>
2023-03-08 20:10:31 +0200

    Testejä lisätty.

--- src/tests/postfix_evaluator_test.py
+++ src/tests/postfix_evaluator_test.py
@@ -21,7 +21,7 @@ class TestPostfixEvaluator(unittest.TestCase):
         """Test if the function calculates '1 2 +' RPN expression to 3.0."""
         self.calculation.result_rpn = '1 2 +'
         self.postfix_evaluator.evaluate(self.calculation)
-        self.assertEqual(self.calculation.result, 3.0)
+        self.assertEqual(self.calculation.result, 3)
 
     def test_basic_subtraction(self):
         """Test if the function calculates '1 2 -' RPN expression to -1.0."""
@@ -33,7 +33,7 @@ class TestPostfixEvaluator(unittest.TestCase):
         """Test if the function calculates '1 2 *' RPN expression to 2.0."""
         self.calculation.result_rpn = '1 2 *'
         self.postfix_evaluator.evaluate(self.calculation)
-        self.assertEqual(self.calculation.result, 2.0)
+        self.assertEqual(self.calculation.result, 2)
 
     def test_basic_division(self):
         """Test if the function calculates '1 2 /' RPN expression to 0.5."""
@@ -45,13 +45,13 @@ class TestPostfixEvaluator(unittest.TestCase):
         """Test if the function calculates '1 2 ^' RPN expression to 1.0."""
         self.calculation.result_rpn = '1 2 ^'
         self.postfix_evaluator.evaluate(self.calculation)
-        self.assertEqual(self.calculation.result, 1.0)
+        self.assertEqual(self.calculation.result, 1)
 
     def test_negative_values_1(self):
         """Test if the function calculates '-2 2 +' RPN expression to 0.0."""
         self.calculation.result_rpn = '-2 2 +'
         self.postfix_evaluator.evaluate(self.calculation)
-        self.assertEqual(self.calculation.result, 0.0)
+        self.assertEqual(self.calculation.result, 0)
 
     def test_negative_values_2(self):
         """Test if the function calculates '-3.5 2 +' RPN expression to -1.5."""
@@ -63,13 +63,13 @@ class TestPostfixEvaluator(unittest.TestCase):
         """Test if the function calculates '10 5 2 ^ *' RPN expression to 250.0."""
         self.calculation.result_rpn = '10 5 2 ^ *'
         self.postfix_evaluator.evaluate(self.calculation)
-        self.assertEqual(self.calculation.result, 250.0)
+        self.assertEqual(self.calculation.result, 250)
 
     def test_basic_arithmetic_2(self):
         """Test if the function calculates '25 5 5 ^ 5 / *' RPN expression to 15625.0."""
         self.calculation.result_rpn = '25 5 5 ^ 5 / *'
         self.postfix_evaluator.evaluate(self.calculation)
-        self.assertEqual(self.calculation.result, 15625.0)
+        self.assertEqual(self.calculation.result, 15625)
 
     def test_basic_arithmetic_3(self):
         """Test if the function calculates '23 27 + 3 3 * /' RPN expression to 5.555...6."""
@@ -81,7 +81,13 @@ class TestPostfixEvaluator(unittest.TestCase):
         """Test if the function calculates '6 2 3 ^ *' RPN expression to 48."""
         self.calculation.result_rpn = '6 2 3 ^ *'
         self.postfix_evaluator.evaluate(self.calculation)
-        self.assertEqual(self.calculation.result, 48.0)
+        self.assertEqual(self.calculation.result, 48)
+
+    def test_negation_1(self):
+        """Test if the function calculates '6 2 3 ^ *' RPN expression to 48."""
+        self.calculation.result_rpn = '1 -('
+        self.postfix_evaluator.evaluate(self.calculation)
+        self.assertEqual(self.calculation.result, -1)
 
     # FUNCTIONS
 
