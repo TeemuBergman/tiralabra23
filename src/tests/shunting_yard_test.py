adc8e0a0c792cfc849d2569abaca358900a6e2c8
Teemu Bergman <teemu.bergman@helsinki.fi>
2023-03-10 13:47:06 +0200

    Testejä korjattu.

--- src/tests/shunting_yard_test.py
+++ src/tests/shunting_yard_test.py
@@ -19,179 +19,209 @@ class TestShuntingYard(unittest.TestCase):
     # BASIC TESTS
 
     def test_basic_addition(self):
-        """Test if the class converts '1+2' expression to '1 2 +'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('1+2'))
         self.assertEqual(self.calculation.result_rpn, '1 2 +')
 
     def test_basic_subtraction(self):
-        """Test if the class converts '1-2' expression to '1 2 -'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('1-2'))
         self.assertEqual(self.calculation.result_rpn, '1 2 -')
 
     def test_basic_multiplication(self):
-        """Test if the class converts '1*2' expression to '1 2 *'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('1*2'))
         self.assertEqual(self.calculation.result_rpn, '1 2 *')
 
     def test_basic_division(self):
-        """Test if the class converts '1/2' expression to '1 2 /'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('1/2'))
         self.assertEqual(self.calculation.result_rpn, '1 2 /')
 
     def test_basic_exponentitation(self):
-        """Test if the class converts '1^2' expression to '1 2 ^'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('1^2'))
         self.assertEqual(self.calculation.result_rpn, '1 2 ^')
 
     def test_basic_multiplication_with_negative_values(self):
-        """Test if the class converts '-1*(-2)' expression to '1 2 - * -'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('-1*(-2)'))
-        self.assertEqual(self.calculation.result_rpn, '-1 -2 *')
+        self.assertEqual(self.calculation.result_rpn, '0 1 0 2 - * -')
 
     def test_basic_arithmetic_1(self):
-        """Test if the class converts '10.5*(5^2)' expression to '10 5 2 ^ *'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('10.5*(5^2)'))
         self.assertEqual(self.calculation.result_rpn, '10.5 5 2 ^ *')
 
     def test_basic_arithmetic_2(self):
-        """Test if the class converts '25*((5^10)/5)' expression to '25 5 10 ^ 5 / *'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('25*((5^10)/5)'))
         self.assertEqual(self.calculation.result_rpn, '25 5 10 ^ 5 / *')
 
     def test_basic_arithmetic_3(self):
-        """Test if the class converts '23+(-27)/(3*3)' expression to '23 27 - 3 3 * / +'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('23+(-27)/(3*3)'))
-        self.assertEqual(self.calculation.result_rpn, '23 -27 3 3 * / +')
+        self.assertEqual(self.calculation.result_rpn, '23 0 27 - 3 3 * / +')
 
     def test_basic_arithmetic_4(self):
-        """Test if the class converts '(-5+2)^2' expression to '-5 2 + 2 ^'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('(-5+2)^2'))
-        self.assertEqual(self.calculation.result_rpn, '-5 2 + 2 ^')
+        self.assertEqual(self.calculation.result_rpn, '0 5 - 2 + 2 ^')
 
     def test_decimals_1(self):
-        """Test if the class converts '1.5+2.5' expression to '1.5 2.5 +'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('1.5+2.5'))
         self.assertEqual(self.calculation.result_rpn, '1.5 2.5 +')
 
     def test_decimals_2(self):
-        """Test if the class converts '-8.2/-3.7' expression to '-8.2 -3.7 /'"""
-        self.shunting_yard.convert(self.calculation.new('-8.2/-3.7'))
-        self.assertEqual(self.calculation.result_rpn, '-8.2 -3.7 /')
+        """Test if the class converts the given expression correctly to RPN.'"""
+        self.shunting_yard.convert(self.calculation.new('-8.2/(-3.7)'))
+        self.assertEqual(self.calculation.result_rpn, '0 8.2 0 3.7 - / -')
 
     def test_variables_1(self):
-        """Test if the class converts 'x+y' expression to '1 2 +'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('x+y', 'x=1,y=2'))
         self.assertEqual(self.calculation.result_rpn, '1 2 +')
 
     def test_too_many_parentheses_1(self):
-        """Test if the class converts '((((((((((1+1))))))))))' expression to '1 1 +'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('((((((((((1+1))))))))))'))
         self.assertEqual(self.calculation.result_rpn, '1 1 +')
 
     def test_too_many_parentheses_2(self):
-        """Test if the class converts '(((((((((((-1)+(1.010101)))))))))))' correctly.'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('(((((((((((-1)+(1.010101)))))))))))'))
-        self.assertEqual(self.calculation.result_rpn, '-1 1.010101 +')
+        self.assertEqual(self.calculation.result_rpn, '0 1 - 1.010101 +')
 
     def test_negative_parentheses(self):
-        """Test if the class converts '-(1)' correctly.'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('-(1)'))
-        self.assertEqual(self.calculation.result_rpn, '1 -(')
+        self.assertEqual(self.calculation.result_rpn, '0 1 -')
 
     def test_negative_expression_1(self):
-        """Test if the class converts '1-(1)' correctly.'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('1-(1)'))
         self.assertEqual(self.calculation.result_rpn, '1 1 -')
 
     def test_negative_expression_2(self):
-        """Test if the class converts '-(1-(1))' correctly.'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('-(1-(1))'))
-        self.assertEqual(self.calculation.result_rpn, '1 1 - -(')
+        self.assertEqual(self.calculation.result_rpn, '0 1 1 - -')
 
     def test_negative_expression_3(self):
-        """Test if the class converts '-(1/2)-2' correctly.'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('-(1/2)-2'))
-        self.assertEqual(self.calculation.result_rpn, '1 2 / -( 2 -')
+        self.assertEqual(self.calculation.result_rpn, '0 1 2 / - 2 -')
 
     def test_negative_expression_4(self):
-        """Test if the class converts '-(-1-(-1))' correctly.'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('-(-1-(-1))'))
-        self.assertEqual(self.calculation.result_rpn, '-1 -1 - -(')
+        self.assertEqual(self.calculation.result_rpn, '0 0 1 - 0 1 - - -')
 
     def test_negative_expression_5(self):
-        """Test if the class converts the given expression to RPN correctly.'"""
-        self.shunting_yard.convert(self.calculation.new('-(x*(-y^x))', 'x=2, y=2'))
-        self.assertEqual(self.calculation.result_rpn, '2 -2 2 ^ * -(')
+        """Test if the class converts the given expression correctly to RPN.'"""
+        self.shunting_yard.convert(self.calculation.new('-(2*(-2^2))'))
+        self.assertEqual(self.calculation.result_rpn, '0 2 0 2 2 ^ - * -')
 
     def test_negative_expression_6(self):
-        """Test if the class converts the given expression to RPN correctly.'"""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('-2^2'))
-        self.assertEqual(self.calculation.result_rpn, '-2 2 ^')
+        self.assertEqual(self.calculation.result_rpn, '0 2 2 ^ -')
+
+    def test_negative_expression_7(self):
+        """Test if the class converts the given expression correctly to RPN.'"""
+        self.shunting_yard.convert(self.calculation.new('-(1)-1'))
+        self.assertEqual(self.calculation.result_rpn, '0 1 - 1 -')
+
+    def test_negative_expression_8(self):
+        """Test if the class converts the given expression correctly to RPN.'"""
+        self.shunting_yard.convert(self.calculation.new('(-(-2^2))'))
+        self.assertEqual(self.calculation.result_rpn, '0 0 2 2 ^ - -')
+
+    def test_negative_expression_8(self):
+        """Test if the class converts the given expression correctly to RPN.'"""
+        self.shunting_yard.convert(self.calculation.new('1+-1'))
+        self.assertEqual(self.calculation.result_rpn, '1 0 + 1 -')
+
+    def test_negative_expression_8(self):
+        """Test if the class converts the given expression correctly to RPN.'"""
+        self.shunting_yard.convert(self.calculation.new('1+(-1)'))
+        self.assertEqual(self.calculation.result_rpn, '1 0 1 - +')
+
+    def test_negative_expression_9(self):
+        """Test if the class converts the given expression correctly to RPN.'"""
+        self.shunting_yard.convert(self.calculation.new('2*(-2)'))
+        self.assertEqual(self.calculation.result_rpn, '2 0 2 - *')
+
+    def test_negative_expression_9(self):
+        """Test if the class converts the given expression correctly to RPN.'"""
+        self.shunting_yard.convert(self.calculation.new('2/(-2)'))
+        self.assertEqual(self.calculation.result_rpn, '2 0 2 - /')
 
     # FUNCTIONS
 
     def test_function_sine_1(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('sinr(12)'))
         self.assertEqual(self.calculation.result_rpn, '12 sinr')
 
     def test_function_sine_2(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('SINR(12) + 3'))
         self.assertEqual(self.calculation.result_rpn, '12 sinr 3 +')
 
     def test_function_sine_3(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('sinr(3+3)'))
         self.assertEqual(self.calculation.result_rpn, '3 3 + sinr')
 
     def test_function_sine_4(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('sinr(1) + sinr(1)'))
         self.assertEqual(self.calculation.result_rpn, '1 sinr 1 sinr +')
 
     def test_function_sine_5(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('sinr(sinr(1))'))
         self.assertEqual(self.calculation.result_rpn, '1 sinr sinr')
 
     def test_function_cosine_1(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('cosr(12)'))
         self.assertEqual(self.calculation.result_rpn, '12 cosr')
 
     def test_function_cosine_2(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('COSR(12+3)'))
         self.assertEqual(self.calculation.result_rpn, '12 3 + cosr')
 
     def test_function_tangent_1(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('tanr(12)'))
         self.assertEqual(self.calculation.result_rpn, '12 tanr')
 
     def test_function_tangent_2(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('TANR(12+3)'))
         self.assertEqual(self.calculation.result_rpn, '12 3 + tanr')
 
     def test_function_square_root_1(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('sqr(12)'))
         self.assertEqual(self.calculation.result_rpn, '12 sqr')
 
     def test_function_square_root_2(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('SQR(12+3)'))
         self.assertEqual(self.calculation.result_rpn, '12 3 + sqr')
 
     def test_function_logarithm_1(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('log(12)'))
         self.assertEqual(self.calculation.result_rpn, '12 log')
 
     def test_function_logarithm_2(self):
-        """Test if the function converts given expression to a correct RPN."""
+        """Test if the class converts the given expression correctly to RPN.'"""
         self.shunting_yard.convert(self.calculation.new('LOG(12+3)'))
         self.assertEqual(self.calculation.result_rpn, '12 3 + log')
 
